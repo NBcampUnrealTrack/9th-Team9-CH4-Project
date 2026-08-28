22235d84932b80b75d92d0c3902524ae31246cba
huidong8809 <huidong8809@gmail.com>
2026-08-28 17:20:02 +0900

    feat: 탄약관리 컴포넌트, 무기 선택/전환 컴포넌트 완성
    AmmoComponent : 탄약관리
    WeaponManagerComponent : 무기 선택/ 전환
    그리고 무기 폴더 정리해뒀습니다

---
+++ Source/TabulletProject/Component/WeaponManagerComponent.h
@@ -0,0 +1,75 @@
+// WeaponManagerComponent.h
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "TabulletProject/WeaponType.h"
+#include "WeaponManagerComponent.generated.h"
+
+class AWeaponBase;
+class UInputAction;
+struct FInputActionValue;
+
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
+class TABULLETPROJECT_API UWeaponManagerComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UWeaponManagerComponent();
+
+	UFUNCTION(BlueprintCallable, Category = "Weapon")
+	void SwitchWeapon(EWeaponType NewType);
+
+	UFUNCTION(BlueprintCallable, Category = "Weapon")
+	void FireCurrentWeapon();
+
+	UFUNCTION(BlueprintPure, Category = "Weapon")
+	AWeaponBase* GetCurrentWeapon() const { return CurrentWeapon; }
+
+protected:
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Weapon")
+	TSubclassOf<AWeaponBase> RevolverClass;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Weapon")
+	TSubclassOf<AWeaponBase> ShotgunClass;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Weapon")
+	TSubclassOf<AWeaponBase> SniperClass;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Weapon")
+	FName WeaponSocketName = TEXT("WeaponSocket");
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Input")
+	TObjectPtr<UInputAction> Weapon1Action;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Input")
+	TObjectPtr<UInputAction> Weapon2Action;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Input")
+	TObjectPtr<UInputAction> Weapon3Action;
+
+private:
+	void SpawnAllWeapons();
+	void TryBindInput();
+
+	UFUNCTION()
+	void OnWeapon1(const FInputActionValue& Value);
+
+	UFUNCTION()
+	void OnWeapon2(const FInputActionValue& Value);
+
+	UFUNCTION()
+	void OnWeapon3(const FInputActionValue& Value);
+
+	UPROPERTY()
+	TMap<EWeaponType, TObjectPtr<AWeaponBase>> WeaponInstances;
+
+	UPROPERTY()
+	TObjectPtr<AWeaponBase> CurrentWeapon;
+
+	FTimerHandle BindRetryHandle;
+};
