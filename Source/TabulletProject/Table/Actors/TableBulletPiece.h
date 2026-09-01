865d1fa45d29b5694592d5ad0890814e4deaaad2
X_Ylitol <kjw1469641@gmail.com>
2026-09-01 17:47:44 +0900

    refactor: TableBulletPiece 소유권 관리 메서드 재구성

--- Source/TabulletProject/Table/Actors/TableBulletPiece.h
+++ Source/TabulletProject/Table/Actors/TableBulletPiece.h
@@ -26,19 +26,8 @@ public:
 	UFUNCTION(BlueprintPure, Category = "Table Piece")
 	bool IsOut() const	{ return PieceState == ETablePieceState::Out; }
 
-	UFUNCTION(BlueprintPure, Category = "Table Piece")
-	bool IsOwnedBy(const APlayerState* PlayerState) const;
-
-	UFUNCTION(BlueprintPure, Category = "Table Piece")
-	APlayerState* GetOwningPlayerState() const { return OwningPlayerState; }
-
-	UFUNCTION(BlueprintCallable, BlueprintAuthorityOnly, Category = "Table Piece")
-	void SetOwningPlayerState(APlayerState* NewOwningPlayerState);
-	
 	bool ApplyFlickImpulse(const FVector& WorldImpulse);
 
-	bool IsMoving(float LinearThreshold, float AngularThreshold) const;
-	
 	bool IsMoving(float LinearThreshold, float AngularThreshold) const;
 	
 	UFUNCTION(BlueprintCallable, BlueprintAuthorityOnly, Category = "Table | Ownership")
