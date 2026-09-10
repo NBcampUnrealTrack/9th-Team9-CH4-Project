0a135af820d9c6fe1882879a7d57cb78fe47a377
taek2293 <yim240130gmail.com>
2026-09-10 17:49:08 +0900

    사격 턴 진행 수정

--- Source/TabulletProject/TPGameMode.cpp
+++ Source/TabulletProject/TPGameMode.cpp
@@ -672,11 +672,6 @@ void ATPGameMode::AwardAmmoForFallenPiece(ATableBulletPiece* FallenPiece, APlaye
 		return;
 	}
 
-	if (FallenPiece->GetPieceType() != ETablePieceType::Special)
-	{
-		return;
-	}
-
 	ATPCharacter* CapturingCharacter = GetCharacterForPlayerState(CapturingPlayer);
 	UAmmoComponent* AmmoComponent = CapturingCharacter ? CapturingCharacter->FindComponentByClass<UAmmoComponent>() : nullptr;
 	if (!AmmoComponent)
@@ -684,7 +679,10 @@ void ATPGameMode::AwardAmmoForFallenPiece(ATableBulletPiece* FallenPiece, APlaye
 		return;
 	}
 
-	const EWeaponType AmmoType = FallenPiece->GetRewardWeaponType();
+	const EWeaponType AmmoType = FallenPiece->GetPieceType() == ETablePieceType::Special
+		? FallenPiece->GetRewardWeaponType()
+		: EWeaponType::Revolver;
+
 	AmmoComponent->SetAmmoCount(AmmoType, AmmoComponent->GetAmmoCount(AmmoType) + 1);
 	UE_LOG(LogTemp, Log, TEXT("Awarded ammo. Player=%s Type=%d"), *CapturingPlayer->GetPlayerName(), static_cast<int32>(AmmoType));
 }
@@ -722,7 +720,7 @@ void ATPGameMode::BuildShootingTurnOrder()
 
 	for (APlayerState* PlayerState : TPGameState->PlayerArray)
 	{
-		if (IsValid(PlayerState) && PlayerState == TablePhaseWinner && IsPlayerAlive(PlayerState) && HasAnyAmmo(PlayerState))
+		if (IsValid(PlayerState) && IsPlayerAlive(PlayerState) && HasAnyAmmo(PlayerState))
 		{
 			ShootingTurnOrder.Add(PlayerState);
 		}
@@ -730,16 +728,6 @@ void ATPGameMode::BuildShootingTurnOrder()
 
 	ShootingTurnOrder.Sort([this](const TObjectPtr<APlayerState>& Left, const TObjectPtr<APlayerState>& Right)
 	{
-		if (Left == TablePhaseWinner)
-		{
-			return true;
-		}
-
-		if (Right == TablePhaseWinner)
-		{
-			return false;
-		}
-
 		const int32 LeftAmmo = GetTotalAmmoCount(Left.Get());
 		const int32 RightAmmo = GetTotalAmmoCount(Right.Get());
 		if (LeftAmmo != RightAmmo)
@@ -806,7 +794,7 @@ void ATPGameMode::NotifyShotResolved(AController* ShootingController)
 		return;
 	}
 
-	StartNextTableRound();
+	AdvanceShootingTurn();
 }
 
 bool ATPGameMode::CheckShootingGameOver()
