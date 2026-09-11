58183d6a916aedb7bb4ebea7c1a9efbf4a17d1d3
huidong8809 <huidong8809@gmail.com>
2026-09-11 20:55:10 +0900

    feat: 사망시 Ragdoll로 연출/ 캐릭터 에셋 물리판정 부분 수정

--- Source/TabulletProject/Component/HitReactionComponent.cpp
+++ Source/TabulletProject/Component/HitReactionComponent.cpp
@@ -3,7 +3,10 @@
 #include "HitReactionComponent.h"
 #include "HealthComponent.h"
 #include "GameFramework/Character.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "GameFramework/SpringArmComponent.h"
 #include "Components/SkeletalMeshComponent.h"
+#include "Components/CapsuleComponent.h"
 #include "Animation/AnimInstance.h"
 #include "Kismet/GameplayStatics.h"
 // #include "NiagaraFunctionLibrary.h"
@@ -45,30 +48,49 @@ void UHitReactionComponent::HandleHealthChanged(float NewHealth, float MaxHealth
 
 void UHitReactionComponent::HandleDeathVisual()
 {
-	ACharacter* OwnerCharacter = Cast<ACharacter>(GetOwner());
-	if (!OwnerCharacter || !DeathMontage)
-	{
-		OnDeathMontageFinished.Broadcast();
-		return;
-	}
-	
-	UAnimInstance* AnimInstance = OwnerCharacter->GetMesh()->GetAnimInstance();
-	if (!AnimInstance)
-	{
-		OnDeathMontageFinished.Broadcast();
-		return;
-	}
-	
-	AnimInstance->Montage_Play(DeathMontage);
-	
-	FOnMontageEnded EndDelegate;
-	EndDelegate.BindUObject(this, &UHitReactionComponent::OnDeathMontageEnded);
-	AnimInstance->Montage_SetEndDelegate(EndDelegate, DeathMontage);
+	ApplyRagdoll();
+	OnDeathVisualApplied.Broadcast();
 }
 
-void UHitReactionComponent::OnDeathMontageEnded(UAnimMontage* Montage, bool bInterrupted)
+void UHitReactionComponent::ApplyRagdoll()
 {
-	OnDeathMontageFinished.Broadcast();
+	ACharacter* OwnerCharacter = Cast<ACharacter>(GetOwner());
+	if (!OwnerCharacter)
+	{
+		return;
+	}
+
+	// 카메라가 메쉬(래그돌 뼈)를 따라가지 않도록 그 순간 시점을 분리해서 고정
+	if (USpringArmComponent* CameraBoom = OwnerCharacter->FindComponentByClass<USpringArmComponent>())
+	{
+		CameraBoom->DetachFromComponent(FDetachmentTransformRules::KeepWorldTransform);
+		// bUsePawnControlRotation이 켜져 있으면 분리 후에도 마우스 회전을 계속 따라가므로 같이 꺼줌
+		CameraBoom->bUsePawnControlRotation = false;
+		CameraBoom->SetComponentTickEnabled(false);
+	}
+
+	// 캡슐/무브먼트가 더 이상 물리 시뮬레이션에 간섭하지 않도록 정지
+	if (UCharacterMovementComponent* MovementComp = OwnerCharacter->GetCharacterMovement())
+	{
+		MovementComp->DisableMovement();
+		MovementComp->SetComponentTickEnabled(false);
+	}
+
+	if (UCapsuleComponent* Capsule = OwnerCharacter->GetCapsuleComponent())
+	{
+		Capsule->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	}
+
+	// 메쉬를 물리 시뮬레이션으로 전환해 축 처지는 래그돌 연출
+	if (USkeletalMeshComponent* Mesh = OwnerCharacter->GetMesh())
+	{
+		Mesh->SetCollisionProfileName(TEXT("Ragdoll"));
+		Mesh->SetCollisionObjectType(ECC_PhysicsBody);
+		Mesh->SetSimulatePhysics(true);
+		Mesh->SetAllBodiesSimulatePhysics(true);
+		Mesh->WakeAllRigidBodies();
+		Mesh->bBlendPhysics = true;
+	}
 }
 
 void UHitReactionComponent::PlayHitMontage()
